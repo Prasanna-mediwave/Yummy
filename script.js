28a6df82e2e96df3b3285c0b70a786872935be07
Prasanna-mediwave <prasanna@mindwaveventures.com>
2022-01-24 19:04:04 +0530

    updated css desing

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-const Add_Recip = document.querySelector("#Add_Recip");
-
-Add_Recip.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const formData = new FormData(event.target);
-  const nameRecipe = formData.get("nameRecipe");
-  const recipeImage = formData.get("recipeImage");
-  const food_type = formData.get("food_type");
-  const ingredientReq = formData.get("ingredientReq");
-  const prepareStep = formData.get("prepareStep");
-  const movie = {
-    nameRecipe,
-    recipeImage,
-    food_type,
-    ingredientReq,
-    prepareStep,
-  };
-
-  // append movie to html list
-  //   addToMovieList(movie);
-  console.log(movie);
-  // rest form
-  event.target.reset();
+mtUpdate.addEventListener("change", function (event) {
+  console.log("Working------------->");
+  // uploaded_image = this.files[0];
+  imgurl = event.target.files[0].name;
+  document.getElementById("RecipeImg").value = imgurl;
+  path = document.getElementById("mtUpdate").value;
+  console.log(path);
+  console.log(imgurl);
+  //let inpurl = webkitURL(inp);
+  if (imgurl) {
+    console.log(imgurl);
+  } else {
+    console.log("Working but file not uploaded");
+  }
 });
